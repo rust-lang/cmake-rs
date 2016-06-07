101be1471f8579107fb3cff54705eeb456b1f7f7
Valerii Tsyvakin <devcen13@gmail.com>
2016-06-08 01:40:46 +0300

    Add opt_level configuration method

--- src/lib.rs
+++ src/lib.rs
@@ -58,6 +58,7 @@ use std::process::Command;
 pub struct Config {
     path: PathBuf,
     generator: Option<OsString>,
+    opt_level: u32,
     cflags: OsString,
     defines: Vec<(OsString, OsString)>,
     deps: Vec<String>,
@@ -96,6 +97,7 @@ impl Config {
         Config {
             path: env::current_dir().unwrap().join(path),
             generator: None,
+            opt_level: 0,
             cflags: OsString::new(),
             defines: Vec::new(),
             deps: Vec::new(),
@@ -113,6 +115,14 @@ impl Config {
         self.generator = Some(generator.as_ref().to_owned());
         self
     }
+
+
+    /// Sets the optimization level of the generated object files.
+    pub fn opt_level(&mut self, level: u32) -> &mut Config {
+        self.opt_level = level;
+        self
+    }
+
 
     /// Adds a custom flag to pass down to the compiler, supplementing those
     /// that this library already passes.
@@ -203,14 +213,14 @@ impl Config {
         });
         let msvc = target.contains("msvc");
         let c_compiler = gcc::Config::new().cargo_metadata(false)
-                                         .opt_level(0)
+                                         .opt_level(self.opt_level)
                                          .debug(false)
                                          .target(&target)
                                          .host(&host)
                                          .get_compiler();
         let cxx_compiler = gcc::Config::new().cargo_metadata(false)
                                          .cpp(true)
-                                         .opt_level(0)
+                                         .opt_level(self.opt_level)
                                          .debug(false)
                                          .target(&target)
                                          .host(&host)
