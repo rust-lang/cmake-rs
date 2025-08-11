f09de3fb2bd6ce09252a2c216679df70f61a3ea6
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2025-08-11 15:04:52 +0900

    Use `eprintln` instead to print the command running next (#191)
    Fixes: https://github.com/rust-lang/cmake-rs/issues/128

--- src/lib.rs
+++ src/lib.rs
@@ -1076,7 +1076,7 @@ impl Default for Version {
 }
 
 fn run(cmd: &mut Command, program: &str) {
-    println!("running: {:?}", cmd);
+    eprintln!("running: {:?}", cmd);
     let status = match cmd.status() {
         Ok(status) => status,
         Err(ref e) if e.kind() == ErrorKind::NotFound => {
